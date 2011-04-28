bc99e884f1f1af2e38a3ec42209e8d14b77ff6bc
Nermin Serifovic <nermin.s@gmail.com>
2011-04-27 20:51:59 -0400

    Refactoring

--- src/main/scala/org/cs264/gtc/Runner.scala
+++ src/main/scala/org/cs264/gtc/Runner.scala
@@ -8,12 +8,13 @@ import collection.mutable.ListBuffer
 
 object Runner {
   def main(args: Array[String]) = {
-    //val hotSearches = new ListBuffer[String]
-    val today = new LocalDate
+    println(getHotSearches(System.getProperty("number.of.days").toInt))
+  }
 
-    val hotSearches = for (day <- System.getProperty("number.of.days").toInt to 1 by -1)
-                      yield getHotSearches(today.minusDays(day))
-    println(hotSearches.flatten)
+  private def getHotSearches(numOfDays: Int): IndexedSeq[String] = {
+    val today = new LocalDate
+    val hotSearches = for (day <- numOfDays to 1 by -1) yield getHotSearches(today.minusDays(day))
+    hotSearches.flatten
   }
 
   private def getHotSearches(date: LocalDate) = {
